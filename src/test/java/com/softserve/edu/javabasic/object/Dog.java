6f4a49f63aff9d510b467987a60c1a727c98ab73
vkotsupyr <v.kotsupyr@gmail.com>
2017-09-11 23:58:21 +0300

    about 'main' and variables

---
+++ src/test/java/com/softserve/edu/javabasic/object/Dog.java
@@ -0,0 +1,16 @@
+package com.softserve.edu.javabasic.object;
+
+public class Dog {
+	String breed;
+	int age;
+	String color;
+	
+	void barking() {
+	}
+
+	void hungry() {
+	}
+
+	void sleeping() {
+	}
+}
